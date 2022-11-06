6bc5c782ab71fa00e214223b94116b0448b61de7
Joseph Bellahcen <joeclb@icloud.com>
2022-11-06 13:31:59 -0500

    Updated FeatureExtractor and RavdessDataset routines

--- src/Respira/FeatureExtractor.py
+++ src/Respira/FeatureExtractor.py
@@ -5,27 +5,23 @@ import torchaudio
 class FeatureExtractor:
     def __init__(self):
         torch.random.manual_seed(0xbeef)
-        bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+        bundle = torchaudio.pipelines.WAV2VEC2_XLSR53
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = bundle.get_model().to(self.device)
-        self.decoder = GreedyCTCDecoder(bundle.get_labels())
+        self.decoder = GreedyCTCDecoder(torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H.get_labels())
 
         self.sample_rate = bundle.sample_rate
 
-    def __call__(self, audio_path):
-        # Open audio data and resample if necessary
-        waveform, sample_rate = torchaudio.load(audio_path)
-        waveform = waveform.to(self.device)
-
+    def __call__(self, torchaudio_data, sample_rate):
         if sample_rate != self.sample_rate:
-            waveform = torchaudio.functional.resample(waveform, sample_rate, self.sample_rate)
+            waveform = torchaudio.functional.resample(torchaudio_data, sample_rate, self.sample_rate)
 
         # Get logits
         with torch.inference_mode():
             emission, _ = self.model(waveform)
         
-        return emission[0]
+        return emission
 
     def decode(self, emission):
         indices, transcript = self.decoder(emission)
--- src/Respira/RavdessDataset.py
+++ src/Respira/RavdessDataset.py
@@ -1,103 +1,72 @@
 from datasets import load_dataset
-import json
 from progress.bar import Bar
-import torch
-from torch.utils.data import Dataset
 from Respira import FeatureExtractor
+import torch, torchaudio
+from torch.utils.data import Dataset
 
-class RavdessDataClass(Dataset):
-        def __init__(self, features, labels, transform=None, target_transform=None):
-            self.features = features
-            self.labels = labels
-            self.transform = transform
-            self.target_transform = target_transform
+class RavdessDataset(Dataset):
+    def __init__(self, path=None):
+        self.features = []
+        self.labels = []
 
-        def __len__(self):
-            return len(self.labels)
+        if (path == None):
+            self.__populate_from_remote()
+        else:
+            self.__populate_from_disk(path)
 
-        def __getitem__(self, idx):
-            feature = self.features[idx]
-            label = self.labels[idx]
+    def __len__(self):
+        return len(self.labels)
 
-            if self.transform:
-                feature = self.transform(feature)
-            if self.target_transform:
-                label = self.target_transform(label)
+    def __getitem__(self, idx):
+        feature = self.features[idx]
+        label = self.labels[idx]
 
-            return feature, label
+        return feature, label
 
-class RavdessDataset:
-    def __init__(self):
-        self.data = {
-            "features": [],
-            "labels": []
-        }
-
-    def load_from_remote(self):
-        # Download RAVDESS dataset
+    def __populate_from_remote(self):
+        # Download RAVDESS dataset and extract trainm split
+        # TODO: Split into Train(Actors 0-19) and Test(Actors20-24)
         ravdess = load_dataset("narad/ravdess", split="train")
         ravdess = ravdess.train_test_split(test_size=0.2, seed=0xbeef)["train"]
         ravdess = ravdess.remove_columns(["text", "speaker_id", "speaker_gender"])
+        ravdess = ravdess.with_format("torch")
 
-        # Instantiate FeatureExtractor to generate logits for classifier input
+        # Instantiate FeatureExtractor and model to convert audio data to logits
         feature_extractor = FeatureExtractor()
 
-        features = []
-        labels = []
-
-        # Get progress bar
+        # Instantiate progress bar, as process may take some time
         bar = Bar("Building dataset", max=len(ravdess["labels"]), suffix="%(percent).1f%% - %(eta_td)s")
 
+        # Build dataset
+        i = 0
         for audio, label in zip(ravdess["audio"], ravdess["labels"]):
-            # Extract feature/label from dataset
-            path = audio["path"]
-            emission = feature_extractor(path)
+            # Extract audio data and features
+            audio_path = audio["path"]
+            waveform, samplerate = torchaudio.load(audio_path)
+            emission = feature_extractor(waveform, samplerate)
 
-            # Get logits from FeatureExtractor
-            logits, _ = feature_extractor.decode(emission)
+            # Collapse all timesteps into a single feature
+            feature = torch.mean(emission[0], 0)
 
-            logits = [x.item() for x in logits]
-            
-            # Pad or truncate feature to 512-length
-            logits_len = len(logits)
-            if logits_len <= 128:
-                n_add = 128 - logits_len
-                logits += [0] * n_add
-            else:
-                n_remove = logits_len - 128
-                logits = logits[:n_remove]
-
-            features.append(logits)
-            labels.append(label)
+            self.features.append(feature)
+            self.labels.append(label)
 
             bar.next()
         bar.finish()
 
-        # Store internally
-        self.data["features"] = features
-        self.data["labels"] = labels
+    def __populate_from_disk(self, path):
+        data = torch.load(path)
 
-    def load_from_disk(self, input_path):
-        data = json.load(open(input_path, "r"))
-        self.data["features"] = data["features"]
-        self.data["labels"] = data["labels"]
-            
-    def serialize(self, output_path:str):
-        with open(output_path, "w") as outfile:
-            json.dump(self.data, outfile)
+        self.features = data["features"]
+        self.labels = data["labels"]
 
-    def to_pytorch(self):
-        features = self.data["features"]
-        labels = self.data["labels"]
+        print(f"Imported {len(self.features)} features")
 
-        features = torch.FloatTensor(features)
-        labels = torch.LongTensor(labels)
+    def save_to_disk(self, path):
+        with open(path, "w") as outfile:
+            data = {
+                "features": self.features,
+                "labels": self.labels
+            }
 
-        print(labels)
-
-        return RavdessDataClass(features, labels)
-
-if __name__ == "__main__":
-    dataset = RavdessDataset()
-    dataset.load_from_remote()
-    dataset.serialize("dataset.json")
+            torch.save(data, path)
